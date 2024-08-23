f27328b3c1f10936a38391d6dc8f4d07c6dca6a6
MateusDeFreitas <150572494+MateusDeFreitas@users.noreply.github.com>
2024-08-23 01:07:09 -0300

    Meu portfólio V: 1.3.0
    - Adição do adaptação para dispositivos mobiles

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Fução para controlar o segundo menu e apresentar os textos
+
 const targets = document.querySelectorAll('.observar');
 const segundoMenu = document.getElementById('segundoMenu');
 
@@ -30,3 +32,23 @@ const meuObservador = new IntersectionObserver(obeserverCallback, configuracoesO
 targets.forEach(function(target) {
     meuObservador.observe(target);
 });
+
+
+// Fução para controlar menu mobile
+
+let menuAberto = false;
+
+function expandirMenu() {
+    /*
+        Essa função expande e retrai o menu na versão ,obile do site
+    */
+    const menu = document.getElementById("espacoMenu");
+
+    if (!menuAberto) {
+        menu.classList.add('aberto');
+    } else {
+        menu.classList.remove('aberto');
+    }
+
+    menuAberto = !menuAberto;
+}
